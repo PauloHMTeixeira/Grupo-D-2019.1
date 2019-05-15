8e563c301ffff44f52f25afce41f47053152ea1f
Paulo Teixeira <49165506+PauloHMTeixeira@users.noreply.github.com>
2019-05-15 15:09:36 -0300

    Update CSV002.py

--- CSV002.py
+++ CSV002.py
@@ -2,3 +2,4 @@ import os
 cwd = os.getcwd()  # Get the current working directory (cwd)
 files = os.listdir(cwd)  # Get all the files in that directory
 print("Files in '%s': %s" % (cwd, files))
+# programa para ajudar o programador a achar o diretório, pasta e etc.
