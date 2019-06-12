dd67624114e88b39d522735d9b724b9f1ba0680e
Pedro Q. L. de Assis <pqla@cesar.school>
2019-06-12 18:23:43 -0300

    Update CódigoPrincipal.ino

--- CódigoPrincipal.ino
+++ CódigoPrincipal.ino
@@ -13,6 +13,8 @@ const int buttonPin2 = 3;
 const int buttonPin3 = 4;
 const int buttonPin4 = 5;
 const int buttonPin5 = 6;
+//const int buttonPin6 = 7;
+//const int buttonPin7 = 8;
 
 void mensagemInicial()
 {
@@ -21,7 +23,6 @@ void mensagemInicial()
   lcd.print("Aproxime");  
   lcd.setCursor(4,1);
   lcd.print("seu cracha");
-  Serial.print("funcionando");
 }
 
 void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
@@ -37,16 +38,12 @@ void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
     lcd.setCursor(0,1);
     lcd.print("se sentindo ?");
     delay(1000);
-    Serial.println("antes dos botoes");
-    botoes(); 
-    Serial.println("depois dos botoes");
+    botoes();
     //imprime mensagem no LCD e inicia os botões
   }
 
   void mensagemnegada()   //###### QUANDO DÁ ERRO ######
 {
-    Serial.println("Olá!");
-    Serial.println();
     lcd.clear();
     lcd.setCursor(0,0);
     lcd.print(" Cartão Detectado! ");
@@ -61,17 +58,21 @@ void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
     bool apertou= 0; //botão começa com valor booleano "False"
     
     while (!apertou){ // quando for diferente de "False"
-      if(digitalRead(buttonPin1) == HIGH){
+      
+      if(digitalRead(buttonPin1) == HIGH)
+      {
         lcd.clear();
         lcd.print("Estressado");
+        Serial.println("1");
         delay(1000);
         apertou = 1;
-        mensagemInicial();
+       mensagemInicial();
       }
       else if(digitalRead(buttonPin2) == HIGH)
       {
         lcd.clear();
         lcd.print("Ansioso");
+        Serial.println("2");
         delay(1000);
         apertou = 1;
         mensagemInicial();
@@ -80,6 +81,7 @@ void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
       {
         lcd.clear();
         lcd.print("Neutro");
+        Serial.println("3");
         delay(1000);
         apertou = 1;
         mensagemInicial();
@@ -88,6 +90,7 @@ void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
       {
         lcd.clear();
         lcd.print("Feliz");
+        Serial.println("4");
         delay(1000);
         apertou = 1;
         mensagemInicial();
@@ -96,26 +99,25 @@ void mensagemaprovada()   //###### QUANDO CARTÃO É APRESENTADO ######
       {
         lcd.clear();
         lcd.print("Triste");
+        Serial.println("5");
         delay(1000);
         apertou = 1;
         mensagemInicial();
       }
+
     }
 }
   
 void setup() 
 {
   Serial.begin(9600);   // Inicia a serial
-  Serial.print("Aproxime o seu crachá do leitor...");
-  Serial.println();
+  
   pinMode(buttonPin1, INPUT);
   pinMode(buttonPin2, INPUT);
   pinMode(buttonPin3, INPUT);
   pinMode(buttonPin4, INPUT);
   pinMode(buttonPin5, INPUT);
-  //Serial.println("lcd off");
   lcd.begin(16, 2);  //Define o número de colunas e linhas do LCD:
-  //Serial.println("lcd on");
   mensagemInicial();
 }
  
@@ -123,11 +125,14 @@ void loop()
 {
   String ID;
   String tag_lida;
-  if(Serial.available() > 0) //lê a porta serial para um valor de "tag" maior que zero.
+  if(Serial.available() > 0) //lê a porta serial para um valor maior que zero.
   {
-    ID = Serial.readString(); //armazena tag do cracha na variável ID
-  
+    ID = Serial.readString();
+
+    ID = ID.substring(1, 13);
+
     tag_lida = ID;
+    
     Serial.println(ID);
   
     if (tag_lida == ID) 
