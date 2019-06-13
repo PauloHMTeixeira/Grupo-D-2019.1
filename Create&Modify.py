f4651f1dc9ae81147794d0eaeb4cf44f1918b09a
Paulo Teixeira <49165506+PauloHMTeixeira@users.noreply.github.com>
2019-06-13 08:40:53 -0300

    Update Create&Modify.py

--- Create&Modify.py
+++ Create&Modify.py
@@ -38,7 +38,6 @@ def enviaremail():
     message.attach(part2)
 
     context = ssl.create_default_context()
-    print('banana')
     with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
         server.login(sender_email, password)
         server.sendmail(sender_email, receiver_email, message.as_string())
