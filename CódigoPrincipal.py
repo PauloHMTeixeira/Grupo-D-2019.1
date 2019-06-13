6bb026a0cbb7b5dab6194c805fd91b5b0abefca9
Paulo Teixeira <49165506+PauloHMTeixeira@users.noreply.github.com>
2019-06-13 08:45:04 -0300

    Update CódigoPrincipal.py

--- CódigoPrincipal.py
+++ CódigoPrincipal.py
@@ -9,6 +9,46 @@ tag_lida = Arduino.readline() #leitura da ID
 tag_decode = tag_lida.decode('utf-8')
 tag_decode = tag_decode.replace('\r\n', '')
 
+# envio do feedback
+teto = 4
+
+def enviaremail():
+    import smtplib, ssl
+    from email.mime.text import MIMEText
+    from email.mime.multipart import MIMEMultipart
+
+    port = 465
+    password = 'cesarschool123'
+    sender_email = 'humorcesar@gmail.com'
+    receiver_email = data[y]['E-mail']
+    message = MIMEMultipart("alternative")
+    message["Subject"] = "multipart test"
+    message["From"] = sender_email
+    message["To"] = receiver_email
+    textestressado = """\
+        Estamos cientes da sua situação e gostaria bastante de poder ajudar de forma mais presente, entre em contato com a nossa psicóloga,
+         Ana Carolina, via número de telefone 00000-0000
+        """
+    htmlestressado = """\
+        <html>
+            <body>
+                <p>Estamos cientes da sua situação e gostaria bastante de poder ajudar de forma mais presente, entre em contato com a nossa psicóloga,
+         Ana Carolina, via número de telefone 00000-0000
+                </p>
+
+            </body>
+        </html>
+        """
+    part1 = MIMEText(textestressado, "plain")
+    part2 = MIMEText(htmlestressado, "html")
+    message.attach(part1)
+    message.attach(part2)
+
+    context = ssl.create_default_context()
+    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as server:
+        server.login(sender_email, password)
+        server.sendmail(sender_email, receiver_email, message.as_string())
+
 y = tag_decode
 
 print('Sua ID é: {}' .format(y))
@@ -42,6 +82,11 @@ if y in data:
     if buttonpress == "5":
         data[y]['Feliz'] += 1
         print('Feliz')
+        
+    # precisa ajeitar aqui!!
+    if data[y][x] == teto:
+        enviaremail()
+        teto += 4
 else:
     nome = input('Qual seu nome completo (sem "de" e "e")? ')
     nome2 = nome.lower().split(" ")
